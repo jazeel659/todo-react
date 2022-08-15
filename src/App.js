d308fdb9bcee7257a04d26bdb4422ad012ece31d
jazeel659 <jaseel0659@gmail.com>
2022-08-15 17:05:57 +0530

    app initiated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: ["iteam1", "iteam2"],
+      txtContent: "",
+    };
+  }
+  txtChange = (e) => {
+    this.setState({ txtContent: e.target.value });
+  };
+  addItem = (e) => {
+    let currentTxt = this.state.txtContent;
+    let currentItems = this.state.items;
+    currentItems.push(currentTxt);
+    this.setState({ items: currentItems });
+  };
+  dltItem = (k) => {
+    let currentItems = this.state.items;
+    currentItems.splice(k, 1);
+    this.setState({ items: currentItems });
+  };
+  render() {
+    return (
+      <div className="container">
+        <h1>TO DO</h1>
+        <div>
+          <input type="text" onChange={this.txtChange} />
+          <button onClick={this.addItem}>Add</button>
+        </div>
+
+        <ul>
+          {this.state.items.map((item, k) => {
+            return (
+              <li>
+                {item}{" "}
+                <button
+                  onClick={() => {
+                    this.dltItem(k);
+                  }}
+                >
+                  delete
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
-
 export default App;
